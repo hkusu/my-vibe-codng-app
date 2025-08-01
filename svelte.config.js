3188aeeda46c00eda5029d0b3aebf4c63b53b166
Hiroyuki Kusu <kusu0806@gmail.com>
2025-08-01 14:00:54 +0900

    Add workflow

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,20 @@
 import adapter from '@sveltejs/adapter-static';
 
+const dev = process.argv.includes('dev');
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter({
 			pages: 'build',
 			assets: 'build',
 			fallback: undefined,
 			precompress: false,
 			strict: true
-		})
+		}),
+		paths: {
+			base: dev ? '' : process.env.BASE_PATH
+		}
 	}
 };
 
